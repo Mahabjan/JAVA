ca92ec7d4dbe360d7be1bb03ba5178654973c4e8
Mahabjan <166893678+Mahabjan@users.noreply.github.com>
2024-07-10 11:57:57 +0530

    Add files via upload

---
+++ greater.java
@@ -0,0 +1,9 @@
+public class greater{
+    public static void main(String[] args){
+    int a=2,b=3;
+    if(a>b)
+       		 System.out.println("a is big");
+    else
+        	System.out.println("b is big");
+    }
+}
