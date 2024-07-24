cee6e8a6f1fffd760b38b7a5594772afb021263f
Mahabjan <166893678+Mahabjan@users.noreply.github.com>
2024-07-24 10:19:15 +0530

    Add files via upload

---
+++ defaultvalues.java
@@ -0,0 +1,17 @@
+public class defaultvalues{
+	static boolean val1;
+	static float val2;
+	static double val3;
+	static int val4;
+	static String val5;
+	static long val6;
+	public static void main(String[] args){
+		System.out.println("Default values:");
+		System.out.println("val1="+val1);
+		System.out.println("val2="+val2);
+		System.out.println("val3="+val3);
+		System.out.println("val4="+val4);
+		System.out.println("val5="+val5);
+		System.out.println("val6="+val6);
+		}
+	}
